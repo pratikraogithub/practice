91f854a054408ea17ed6837d95061593f71cd28b
pratikrao <pratikrao11082002@gmail.com>
2023-02-18 09:41:06 +0530

    add file 'files'

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
+import 'dart:convert';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(30, (index) => catalogModel.items[0]);
